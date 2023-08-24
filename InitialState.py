d3cdf664df7324e4c7a8f34c0842347d78de3164
Aaditya Rudra <adityarudra02@gmail.com>
2023-08-25 00:11:28 +0530

    State Initialization class

---
+++ InitialState.py
@@ -0,0 +1,50 @@
+import torch
+import numpy as np
+
+class Initial_State():
+
+    def __init__(self, d_level, No_qudits, device):
+        self.D = d_level
+        self.N = No_qudits
+        self.device = device
+
+    def rho_reshape(self, rho):
+        rho = rho.reshape(1, 1, self.D**self.N, self.D**self.N)
+        return rho
+    
+    def real_custom(self, Q_):
+        psi = torch.zeros((self.D**self.N, 1), dtype = torch.cfloat, device = self.device)
+
+        for qudit in Q_:
+            psi[qudit] = 1
+
+        psi/= torch.norm(psi)
+        rho = torch.matmul(psi, psi.t().conj())
+
+        return self.rho_reshape(rho)
+    
+    def real_random(self):
+        G = np.random.normal(0, 1, size = (self.D**self.N, self.D**self.N))
+        G = torch.tensor(G, dtype = torch.cfloat, device = self.device)
+        rho = torch.matmul(G, G.t().conj())/torch.trace(torch.matmul(G, G.t().conj()))
+
+        return self.rho_reshape(rho)
+    
+    def complex_random(self):
+        G = np.zeros((self.D**self.N, self.D**self.N), dtype = np.complex_)
+        for i in range(self.D**self.N):
+            for j in range(self.D**self.N):
+                G[i][j] = complex(np.random.normal(0, 1, size= None), np.random.normal(0, 1, size= None))
+
+            G = torch.tensor(G, dtype = torch.cfloat, device = self.device)
+            rho = torch.matmul(G, G.t().conj())/torch.trace(torch.matmul(G, G.t().conj()))
+
+        return self.rho_reshape(rho)
+    
+    def complex_custom(self, psi):
+
+        psi = torch.tensor(psi, dtype = torch.cfloat, device = self.device)
+        psi/= torch.norm(psi)
+        rho = torch.matmul(psi, psi.t().conj())
+
+        return self.rho_reshape(rho)
