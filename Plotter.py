7904e9f9a40a867d981485c4c57b274782c98eb4
Aaditya Rudra <adityarudra02@gmail.com>
2023-10-22 11:43:48 +0530

    Removed class and converted to a function

--- Plotter.py
+++ Plotter.py
@@ -2,37 +2,35 @@ from matplotlib import pyplot as plt
 import os
 import json
 
-class Plotter():
+def plotter(D_list, N):
 
-    def plotter(self, D_list, N):
+    file_name = "./Time_Results/N_" + str(N) + ".json"
+    with open(file_name, 'r') as f:
+        label_result = json.load(f)
+    
+    time_result = label_result[0]
+    
+    label = label_result[1]
+    
 
-        file_name = "./Time_Results/N_" + str(N) + ".json"
-        with open(file_name, 'r') as f:
-            label_result = json.load(f)
+    for index,_ in enumerate(D_list):
         
-        time_result = label_result[0]
-        
-        label = label_result[1]
-        
+        plt.plot(label, time_result[index], marker = 'o')
+        plt.xticks(label)
+        plt.legend(D_list)
+        plt.xlabel("No of Qubits traced")
+        plt.ylabel("time")
+        plt.title("N = " + str(N))
 
-        for index,_ in enumerate(D_list):
-            
-            plt.plot(label, time_result[index], marker = 'o')
-            plt.xticks(label)
-            plt.legend(D_list)
-            plt.xlabel("No of Qubits traced")
-            plt.ylabel("time")
-            plt.title("N = " + str(N))
-
-        newpath = r'./Plots' 
-        if not os.path.exists(newpath):
-            os.makedirs(newpath)
-        
-        plt.savefig("./Plots/N_" + str(N) + ".png")
-        plt.clf()
+    newpath = r'./Plots' 
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    
+    plt.savefig("./Plots/N_" + str(N) + ".png")
+    plt.clf()
 
 D_list = [2]
 N_list = list(range(3, 20))
 
 for N in N_list:
-    Plotter().plotter(D_list, N)
+    plotter(D_list, N)
