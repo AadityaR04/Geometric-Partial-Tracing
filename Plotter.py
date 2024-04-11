07cfc2a55c51245e0bdafeb98967718c776714bd
Aaditya Rudra <adityarudra02@gmail.com>
2024-04-11 21:45:13 +0530

    Changes to formatting parameters

--- Plotter.py
+++ Plotter.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import os
 import json
+import matplotlib as mpl
 
 def plotter(D_list, N):
     '''
@@ -22,25 +23,36 @@ def plotter(D_list, N):
     time_result = label_result[0]
     
     label = label_result[1]
+
+    plt.figure(figsize=(15, 10))
     
 
     for index,_ in enumerate(D_list):
         
-        plt.plot(label, time_result[index], marker = 'o', label = "D = " + str(D_list[index]))
+        mpl.rcParams['axes.linewidth'] = 2
+        mpl.rcParams['patch.linewidth'] = 2
+        # mpl.rcParams['patch.edgecolor'] = 'black'
+        # Change opacity of legend edge
+        plt.rcParams['legend.edgecolor'] = 'black'
+
+
+        plt.plot(label, time_result[index], marker = 'o', label = "D = " + str(D_list[index]), linewidth = 2.0)
+        plt.tick_params(axis='both', which='major', labelsize=30)
         plt.xticks(label)
-        plt.legend()
-        plt.xlabel("No of Qubits traced")
-        plt.ylabel("time")
-        plt.title("N = " + str(N))
+        plt.legend(fontsize=40)
+        plt.xlabel("No of Qubits traced", fontsize=30)
+        plt.ylabel("time", fontsize=30)
+        plt.title("N = " + str(N), fontsize=40)
 
     newpath = r'./Plots' 
     if not os.path.exists(newpath):
         os.makedirs(newpath)
     
+    plt.gcf().set_dpi(300)
     plt.savefig("./Plots/N_" + str(N) + ".png")
     plt.clf()
 
-D_list = [2, 3, 4, 5, 6]
+D_list = [2]
 N_list = list(range(3, 16))
 
 for N in N_list:
